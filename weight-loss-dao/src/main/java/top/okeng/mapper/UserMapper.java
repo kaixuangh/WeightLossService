50355ee56b5d7bda864e10289894dcacfbc64216
ray.qian <onlinelei@sina.cn>
2026-01-30 18:20:31 +0800

    upgrade dao path

--- weight-loss-dao/src/main/java/top/okeng/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package top.okeng.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import top.okeng.entity.User;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
